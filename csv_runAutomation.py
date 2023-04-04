1ccb676b01811c5d83fc362a2c089130263774d1
David Crary <22047656+crary@users.noreply.github.com>
2023-04-03 17:15:23 -0700

    Update csv_runAutomation.py

--- csv_runAutomation.py
+++ csv_runAutomation.py
@@ -11,6 +11,31 @@ boot_delay = "120s"
 pac_rec_path = r""
 pac_conf_path = r""
 
+## Setup Logging 
+### create timestamped file for logging
+logDir = "logs/"
+timestr = time.strftime("%m-%d-%H%M")
+logfile = open(f"logs/{timestr}_logs.txt", "a")
+logfile.close()
+
+logfiles = os.listdir(logDir)
+logFile = str(logfiles[-1])
+logging.basicConfig(
+                    filename=f"{logDir}{logFile}",
+                    level=logging.DEBUG,
+                    format="%(asctime)s:%(levelname)s:%(message)s"
+                    )
+
+## Program Variables
+csvFile = "run_template.csv"
+ip1 = "192.168.3.149"
+ip2 = "192.168.3.42"
+linux_host = "10.23.15.214"
+boot_delay = "30s"
+pac_rec_path = r"F:\\ADLP-REDRIX-DVT2_OCH-6761"
+pac_conf_path = r"F:\\ADLP-REDRIX-DVT2_OCH-6761\\0 .Config\\Redrix-DVT_OCH-6761_sys_rail_config_r2.csv"
+driverless= True
+
 ## Create Vars variable file
 # Writes to vars file so that variable are accesable globally within program
 print("Creating variable file...")
@@ -24,18 +49,21 @@ with open("vars.py", "w") as varsfile:
     + "boot_delay = " +  "'" + boot_delay + "'" + "\n"
     )
     varsfile.close()
-print("Done creating variable file...") 
+print("Done creating variable file...")
+logging.debug("Variable file created") 
 
 ## Frontload stop script
-stopScript = "bash -c; sshpass -p xxxxx ssh -o StrictHostKeyChecking=no moo@" + str(linux_host) +  str(' "') \
+stopScript = "bash -c; sshpass -p Qwerty123! ssh -o StrictHostKeyChecking=no oem@" + str(linux_host) +  str(' "') \
 + ". ~/Desktop/Automation_linux/copy_stopScript.sh " + str(ip1) +  str('"')
 subprocess.run(stopScript, shell=True)
 print("Stop script loaded onto DUT ", ip1)
-
+logging.debug(f"Stop script loaded onto DUT:{ip1}") 
 
 ## Print the number of lines in the choosen CSV file
 kpi_csv = pd.read_csv(csvFile)
-print("file has " + str(len(kpi_csv)) + " lines")
+csvLen = str(len(kpi_csv))
+print("CSV file has " + csvLen + " lines")
+logging.debug(f"CSV file has {csvLen} lines") 
 
 ## Reset count files to 1
 kpis = []
@@ -44,6 +72,9 @@ kpis.append("lvp_num.txt")
 kpis.append("plt_num.txt")
 kpis.append("sOix_num.txt")
 kpis.append("meets_num.txt")
+kpis.append("run_counter_ido_soc.txt")
+kpis.append("run_counter_lvp_soc.txt")
+kpis.append("run_counter_plt.txt")
 
 
 for i in range(len(kpis)):
@@ -53,40 +84,87 @@ for i in range(len(kpis)):
 
 ## define kpis to run
 ## No Reboot PACS IDO
-def nrIDO():
-    nrIDO = 'bash ./start_nr-ido.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
-    subprocess.run(nrIDO, shell=True)
-    print ("Starting NR_IDO on Linux Host...")
-    print("NR_IDO running on host: ", linux_host )
+def nrIDO(runtype):
+    if runType == "pac":
+        print ("Starting NR_IDO running on host: ", linux_host)
+        logging.debug(f"Starting NR_IDO running on host:{linux_host}")
+        nrIDO = 'bash ./start_nr-ido.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(nrIDO, shell=True)
+    elif runType == "soc":
+        print("Starting NR_IDO SOCWATCH collection, running on host: ", linux_host)
+        logging.debug(f"Starting NR_IDO SOCWATCH collection, running on host: {linux_host}")
+        ido_soc = 'cmd.exe /C python.exe "run_pacs_NR-IDO_wSOC.py"'
+        subprocess.run(ido_soc, shell=True)
+
 
 ## LVP PACS 
-def lvp():
-    print ("Starting LVP on Linux Host...")
-    print("LVP running on host: ", linux_host )
-    runLvp = 'bash ./start_LVP.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
-    subprocess.run(runLvp, shell=True)
+def lvp(runtype):
+    if runType == "pac":
+        print ("Starting LVP running on host: ", linux_host)
+        logging.debug(f"Starting LVP running on host: {linux_host}")
+        runLvp = 'bash ./start_LVP.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(runLvp, shell=True)
+    elif runType == "soc":
+        print("Starting LVP SOCWATCH collection running on host: ", linux_host)
+        logging.debug(f"Starting LVP SOCWATCH collection running on host: {linux_host}")
+        lvp_soc = 'bash ./soc_LVP.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(lvp_soc, shell=True)
 
 ## PLT PACS
-def plt():
-    print ("Starting PLT on Linux Host...")
-    print("PLT running on host: ", linux_host )
-    runPlt = 'bash ./start_PLT.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
-    subprocess.run(runPlt, shell=True)
+def plt(runtype):
+    if runType == "pac":
+        print ("Starting PLT running on host: ", linux_host )
+        logging.debug(f"Starting PLT running on host: {linux_host}" )
+        runPlt = 'bash ./start_PLT.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(runPlt, shell=True)
+    elif runType == "soc":
+        print("Starting PLT SOCWATCH collection running on host: ", linux_host)
+        logging.debug(f"Starting PLT SOCWATCH collection running on host: {linux_host}")
+        plt_soc = 'bash ./soc_PLT.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(plt_soc, shell=True)
 
 ## Gmeet
-def gmeet():
-    print ("Starting gmeet on Linux Host...")
-    print("Gmeet running on host: ", linux_host )
-    gmeets = "bash ./start_gmeets.sh " + str(ip1) + ' ' + str(ip2) + ' ' + str(linux_host) + ' ' + str(boot_delay)
-    subprocess.run(gmeets) 
+def gmeet(runtype):
+    if runType == "pac":
+        print ("Starting gmeet on Linux Host running on host: ", linux_host)
+        logging.debug(f"Starting gmeet on Linux Host running on host: {linux_host}")
+        gmeets = "bash ./start_gmeets.sh " + str(ip1) + ' ' + str(ip2) + ' ' + str(linux_host) + ' ' + str(boot_delay)
+        subprocess.run(gmeets)
+    elif runType == "soc": 
+        print("Starting GMEET SOCWATCH collection running on host: ", linux_host)
+        logging.debug(f"Starting GMEET SOCWATCH collection running on host: {linux_host}")
+        gmeet_soc = 'cmd.exe /C python.exe "run_pacs_GMEET_wSOC.py"'
+        subprocess.run(gmeet_soc, shell=True)
 
 ## S0ix PACS
 def s0ix():
-    print ("Starting s0ix on Linux Host...")
-    print("s0ix running on host: ", linux_host )
+    print ("Starting s0ix running on host: ", linux_host)
+    logging.debug(f"Starting s0ix running on host: {linux_host}")
     soixRun = 'bash ./prep_s0ix.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay)
     subprocess.run(soixRun, shell=True) 
 
+
+## Front load socwatch
+with open ( csvFile ) as templateFile:
+    template = csv.DictReader( templateFile, dialect='excel', delimiter=',' )
+
+    for row in template:
+        if row['run_type'] == 'soc' and driverless == True:
+            print("Socwatch run found")
+            print("Installing driverless socwatch ", str(ip1))
+            logging.debug(f"Installing driverless socwatch {str(ip1)}")
+            installDriverless = 'bash ./csv_socFunctions.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay) + ' ' + str('true')
+            subprocess.run(installDriverless, shell=True)
+            socInstall = True 
+            break
+        if row['run_type'] == 'soc' and driverless == False:
+            print("Installing Custom Socwatch")
+            logging.debug(f"Installing Custom Socwatch {str(ip1)}")
+            installDriverless = 'bash ./csv_socFunctions.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay) + ' ' + str('false')
+            subprocess.run(installDriverless, shell=True)
+            socInstall = False
+            break
+templateFile.close()
 
 ## Import KPIs from CSV file and run KPIs row by row
 with open ( csvFile ) as templateFile:
@@ -101,21 +179,21 @@ with open ( csvFile ) as templateFile:
 
         def run_kpi(kpi, iterate, runType ):
             #print(kpi + iterate + runType)
-            if kpi == 'ido' and runType == 'pac':
+            if kpi == 'ido':
                 for _ in range(int(iterate)):
-                    nrIDO()
+                    nrIDO(runType)
 
-            elif kpi == 'lvp' and runType == 'pac':
+            elif kpi == 'lvp':
                 for _ in range(int(iterate)):
-                    lvp()
+                    lvp(runType)
 
-            elif kpi == 'plt' and runType == 'pac':
+            elif kpi == 'plt':
                 for _ in range(int(iterate)):
-                    plt()
+                    plt(runType)
 
-            elif kpi == 'gmeet' and runType == 'pac':
+            elif kpi == 'gmeet':
                 for _ in range(int(iterate)):
-                    gmeet()            
+                    gmeet(runType)            
 
             elif kpi == 's0ix' and runType == 'pac':
                 for _ in range(int(iterate)):
@@ -125,3 +203,10 @@ with open ( csvFile ) as templateFile:
 
         run_kpi(kpi, iterate, runType)
 
+    if socInstall == True:
+        logging.debug(f"Copying socwatch files from DUT to Windows host")
+        copySocFiles = 'bash ./csv_socFunctions.sh ' + str(ip1) + ' ' + str(linux_host) + ' ' + str(boot_delay) + ' ' + str('copy')
+        subprocess.run(copySocFiles, shell=True)
+        logging.debug(f"Socwatch files copied to Windows Host")
+
+exit()
